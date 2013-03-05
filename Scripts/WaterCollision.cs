87f3a76a151fccac52e4dda52a7261161c5c6f10
Julie De Lorenzo <delorenzo.julie@gmail.com>
2013-03-04 21:04:25 -0500

    added water collision

--- Scripts/WaterCollision.cs
+++ Scripts/WaterCollision.cs
@@ -1,8 +1,21 @@
 public class WaterCollision : MonoBehavior {
 	
 	void OnCollisionEnter() {
-		
+			// Player fall out!
+	if (other.GetComponent (ThirdPersonStatus))
+	{
+		other.GetComponent (ThirdPersonStatus).FalloutDeath();
+	}
+	// Kill all rigidibodies flying through this area
+	// (Props that fell off)
+	else if (other.attachedRigidbody)
+		Destroy(other.attachedRigidbody.gameObject);
+	// Also kill all character controller passing through
+	// (enemies)
+	else if (other.GetType() == typeof(CharacterController))
+		Destroy(other.gameObject);
 		
 	}
+	
 	
 }
