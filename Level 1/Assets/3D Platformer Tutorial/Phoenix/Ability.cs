5dc6654f6c69d5d2bca291f1fadb5c42f99a33bf
Joe Manzella <JoMama9998@aim.com>
2013-03-24 02:27:36 -0400

    1.2.2
    commit whateverthefuck is new to make git happy <3

---
+++ Level 1/Assets/3D Platformer Tutorial/Phoenix/Ability.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using System.Collections;
+public class Ability : MonoBehaviour{
+	public GameObject[] Player = new GameObject[5];
+	public GameObject obj;
+	public int current_player;
+	public GameObject flame;
+
+	void Start() {
+	}
+
+	void Update() {
+
+		for (var i = 0; i < 5; i++) {
+			if (Player[i].activeSelf) { 
+				current_player = i;
+				break;
+			}
+		}
+
+		if (Input.GetKeyDown("e")) {
+			switch (current_player) {
+				case 0:  //egg
+				//the egg has no abilities
+				break;
+
+				case 1:  //chick
+				//the chick just chirps
+				break;
+
+				case 2:  //adolescent
+				//the adolescent bursts forward
+				break;
+
+				case 3: //adult
+				break;
+
+				case 4:  //elder
+				flame.renderer.enabled = true;
+				break;
+			}
+		}
+
+		if (Input.GetKeyUp("e")) {
+			switch (current_player) {
+				case 0:  //egg
+				//the egg has no abilities
+				break;
+
+				case 1:  //chick
+				//the chick just chirps
+				break;
+
+				case 2:  //adolescent
+				//the adolescent bursts forward
+				break;
+
+				case 3: //adult
+				break;
+
+				case 4:  //elder
+				flame.renderer.enabled = false;
+				break;
+			}
+		}
+	}
+
+
+}
