a276556d0cf8b17d9e43d2f55a8a382623c194e0
Julie De Lorenzo <delorenzo.julie@gmail.com>
2013-04-04 15:24:37 -0400

    stuff

--- Level 1/Assets/Gate.cs
+++ Level 1/Assets/Gate.cs
@@ -5,7 +5,7 @@ public class Gate : MonoBehaviour {
 	public GameObject obj;
 	public Inventory inv;
 	public GameObject gate;
-	void OnCollisionEnter(Collision collision) {
+	/*void OnCollisionEnter(Collision collision) {
 		obj = collision.gameObject;
 		Debug.Log("I'm colliding with " + collision.gameObject.name);
 		if (collision.gameObject.CompareTag ("Player")) {
@@ -15,6 +15,19 @@ public class Gate : MonoBehaviour {
 				gameObject.SetActive(false);
 			}
 		}
+	}
+	*/
+	void OnTriggerEnter(Collider col) {
+		obj = col.gameObject;
+		Debug.Log("I'm colliding with " + col.gameObject.name);
+		if (col.gameObject.CompareTag ("Player")) {
+			if (inv.hasItem("Key")) {
+				inv.removeItem("Key");
+				gate.SetActive(true);
+				gameObject.SetActive(false);
+			}
+		}
+		
 	}
 	
 }
